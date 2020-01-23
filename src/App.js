dfd92ca8e7fc15ed9459c759b6c861309f1bcaa5
YoavBZ <yoav.benzvi@gmail.com>
2020-01-23 19:28:23 +0100

    nm

--- src/App.js
+++ src/App.js
@@ -10,16 +10,40 @@ class App extends React.Component {
 
 		this.state = {
 			weather: null,
+			days: [],
 		}
 	}
 
-	getWeather = (city, country) => {
-		fetch(`'http://api.openweathermap.org/data/2.5/forecast?q=berlin,%20germany?id=524901&APPID=e8225be1aa07c8310717e23a670d52f2'`)
+	getWeather = async (city, country) => {
+		const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}?id=524901&APPID=e8225be1aa07c8310717e23a670d52f2`)
 		.then(response => response.json())
 		.then(data => this.setState({
 			weather: data
 		}))
+		this.setDays()
+	}
 
+	keToCe = () => {
+
+	}
+
+	setDays = () => {
+		const newDays = [];
+		for(let i = 0; i < 7; i++) {
+			newDays.push([this.keToCe(this.state.weather.list[i].main.temp_max), (this.state.weather.list[i].main.temp_min)])
+		}
+		this.setState({
+			days: newDays
+		})
+
+		this.renderDays()
+	}
+
+	renderDays = () => {
+		const renderedDays = this.state.days.map((item, i) => <DayCard key={i} max={this.state.days[0]} min={this.state.days[1]} />)
+		this.setState({
+			days: renderedDays
+		})
 	}
 
 	render() {
@@ -27,6 +51,7 @@ class App extends React.Component {
 			<div className="container">
 				<TopPart getWeather={this.getWeather}/>
 				<div className="days">
+					{this.state.days}
 				</div>
 			</div>
 		)
